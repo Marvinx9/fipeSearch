2c6428774ee17d49491d210f57405040dbbb57c3
afranio dantas <afraniodantas0224@gmail.com>
2025-05-08 23:53:17 -0300

    feat: add list-modelos

--- src/main/java/com/example/fipe/principal/Principal.java
+++ src/main/java/com/example/fipe/principal/Principal.java
@@ -7,7 +7,10 @@ import com.example.fipe.model.Modelo;
 import com.example.fipe.service.ConsumoApi;
 import com.example.fipe.service.ConverteDados;
 import com.example.fipe.service.LimpaString;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 import java.util.Scanner;
@@ -19,7 +22,7 @@ public class Principal {
     private LimpaString limpaString = new LimpaString();
     private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
 
-    public void menu() {
+    public void menu() throws JsonProcessingException {
         System.out.println("**** OPÇÕES ****");
         System.out.println("\nCarros\n\nMotos\n\nCaminhões\n\nDigite uma das opções para consultar valores:");
         var tipoVeiculo = leitura.nextLine();
@@ -38,8 +41,12 @@ public class Principal {
         String enderecoModelos = enderecoMarca + "/" + codigo + "/modelos";
         var jsonModelos = consumo.obterDados(enderecoModelos);
 
-        System.out.println(jsonModelos);
-        List<DadosModelo> dadosmodelos = conversor.obterDados(jsonModelos, new TypeReference<>() {});
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(jsonModelos);
+        JsonNode modelosnode = rootNode.get("modelos");
+
+        String modelosjsonstring = modelosnode.toString();
+        List<DadosModelo> dadosmodelos = conversor.obterDados(modelosjsonstring, new TypeReference<>(){});
 
         List<Modelo> modelos = dadosmodelos.stream().map(Modelo::new).toList();
         modelos.forEach(System.out::println);
